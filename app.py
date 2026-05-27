72dbb6a5e8c18d8a150129b13824df0d2014755d
jeisonvillamil25-sketch <jeisonvillamil25@gmail.com>
2026-05-27 20:36:56 +1000

    Update app.py

--- app.py
+++ app.py
@@ -242,13 +242,15 @@ if uploaded_file:
         start = convert_time_to_minutes(df_dia.iloc[i]["hora inicio"])
         end = convert_time_to_minutes(df_dia.iloc[i]["hora fin"])
         time_windows.append((start, end))
-    if len(df_dia) > MAX_CLIENTES:10
+        
+    if len(df_dia) > MAX_CLIENTES:
         st.warning(f"⚠️ Solo se procesarán {MAX_CLIENTES} clientes")
     warehouse = "51 Nelson Rd, Yennora NSW 2161, Australia"
     locations = [warehouse] + locations
     time_windows = [(0, 1440)] + time_windows
     
     
+
     if optimizar:
         st.success("✅ Rutas optimizadas correctamente")
         st.session_state.optimizado = True
@@ -256,6 +258,7 @@ if uploaded_file:
     if st.session_state.optimizado:
         st.write("TOTAL LOCATIONS:", len(locations))
         st.write(locations)
+    
         distance_matrix = get_distance_matrix(locations)
         routes = optimize_routes(distance_matrix, time_windows, num_vehicles)
 
@@ -362,3 +365,4 @@ Optimiza entregas, reduce tiempos y mejora la eficiencia operativa.
         )
 
 st.caption("Desarrollado por Jeison Villamil 🚀")
+)
